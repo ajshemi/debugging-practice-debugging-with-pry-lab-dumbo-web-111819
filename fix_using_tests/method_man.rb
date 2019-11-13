36d7c27324a65bbc0c334a7dda0c2765409ba3e0
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-13 16:52:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def start_game(player1, player2)
-  result="Hello #{player1} & #{playar2}"
+  result="Hello #{player1} & #{player2}"
   result
 end
 
-def play_game(player2)
+def play_game(player1,player2)
   result="#{player1} is better than #{player2}"
   result
 end
