69e8aa12b6363b9f9d9c0d3a0eb8e9cae9bfc09c
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-13 16:08:25 +0000

    Automatically backed up by Learn

--- first_priority/you_called.rb
+++ first_priority/you_called.rb
@@ -1,7 +1,9 @@
 
 
-super_mario_party
+#super_mario_party
 
 def super_mario_party
-  "Sorry, your princess is in another castle"
+  output="Sorry, your princess is in another castle"
+  output
+  #binding.pry
 end
